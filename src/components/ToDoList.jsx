384593bb26399e0fd70bc78b2e59d4d649dac8e4
Igah Emmanuel <igah.emmanuel@gmail.com>
2024-06-07 15:30:01 +0100

    add: initial files

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Form from './Form';
+import {v4 as uuidv4 } from 'uuid';
+import ToDo from './ToDo';
+import Edit from './Edit';
+uuidv4()
+
+const ToDoList = () => {
+    const [todoValue, setTodo] = useState([]);
+
+    const createTodo = todo => {
+        setTodo([...todoValue, { id: uuidv4(), task: todo, isEditing: false }]);
+    }
+ 
+    const deleteTodo = id => {
+        setTodo(todoValue.filter(todo => todo.id !== id))
+    }
+
+    const editTodo = id => {
+        setTodo(todoValue.map(todo => todo.id === id ? {...todo, isEditing:!todo.isEditing}: todo))
+    }
+
+    const editTask = (task, id) => {
+        setTodo(todoValue.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
+    }
+
+    return (
+        <div className='container bg-gray-700 p-8 mt-20 rounded-md'>
+            <Form createTodo={createTodo} />
+            {
+                todoValue.map((todo, idx) => (
+                    todo.isEditing ? (
+                        <Edit key={idx} editTodo={editTask} task={todo}/> 
+                    ) : (
+                         <ToDo task={todo} key={idx} deleteTodo={deleteTodo} editTodo={editTodo}/>
+                    )
+                ))
+            }
+        </div>
+    );
+}
+
+export default ToDoList;
