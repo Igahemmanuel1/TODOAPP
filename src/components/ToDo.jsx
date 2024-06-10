7f510079de99cc7b83f4647adaaf5105bf95e100
Igah Emmanuel <igah.emmanuel@gmail.com>
2024-06-10 14:55:49 +0100

    update

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,9 +4,9 @@ import { BsFillTrashFill } from 'react-icons/bs'
 
 const ToDo = ({task, deleteTodo, editTodo}) => {
   return (
-    <div className='flex justify-between items-center bg-violet-800 text-white py-3 px-4 rounded-md mb-1 cursor-pointer'>
-        <p className='font-primary'>{task.task}</p>
-        <div className='flex items-center gap-x-4'>
+    <div className='flex justify-between items-center bg-gray-800 text-white py-3 px-4 rounded-md mb-1 cursor-pointer'>
+        <p classname='font-primary'>{task.task}</p>
+        <div className='ml-3 flex items-center gap-x-4'>
           <AiFillEdit className='text-xl' onClick={() => editTodo(task.id)}/>
           <BsFillTrashFill className='text-xl' onClick={() => deleteTodo(task.id)}/>
         </div>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,6 +3,7 @@ import Form from './Form';
 import {v4 as uuidv4 } from 'uuid';
 import ToDo from './ToDo';
 import Edit from './Edit';
+import DateTime from './DateTime';
 uuidv4()
 
 const ToDoList = () => {
@@ -25,18 +26,25 @@ const ToDoList = () => {
     }
 
     return (
-        <div className='container bg-gray-700 p-8 mt-20 rounded-md'>
-            <Form createTodo={createTodo} />
-            {
-                todoValue.map((todo, idx) => (
-                    todo.isEditing ? (
-                        <Edit key={idx} editTodo={editTask} task={todo}/> 
-                    ) : (
-                         <ToDo task={todo} key={idx} deleteTodo={deleteTodo} editTodo={editTodo}/>
-                    )
-                ))
-            }
+      <div className='bg-gray-700 p-8 h-screen'>
+        <DateTime />
+        <header className='text-white text-5xl text-center'>ToDo List</header>
+        <div className="mt-8 flex flex-col items-center justify-center">
+          <Form createTodo={createTodo} />
+          {todoValue.map((todo, idx) =>
+            todo.isEditing ? (
+              <Edit key={idx} editTodo={editTask} task={todo} />
+            ) : (
+              <ToDo
+                task={todo}
+                key={idx}
+                deleteTodo={deleteTodo}
+                editTodo={editTodo}
+              />
+            )
+          )}
         </div>
+      </div>
     );
 }
 
